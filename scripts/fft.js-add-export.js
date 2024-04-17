9d8e4d7fd2bd69a8e830533848deb8fe980b3f8e
Thomas <thurtig.email@gmail.com>
2024-04-17 11:53:18 +0200

    add license text to fft.js file

--- scripts/fft.js-add-export.js
+++ scripts/fft.js-add-export.js
@@ -7,5 +7,12 @@ const fft = fs.readFileSync(fftPath, "utf8");
 // replace module.exports with es6 export
 fftExport = fft.replace("module.exports = FFT;", "export default FFT;");
 
+// read license text
+const licensePath = path.join(__dirname, "./fft.js-license");
+const license = fs.readFileSync(licensePath, "utf8");
+
+// write fft.js with license
+fftExport = license + fftExport;
+
 // write fft.js with export
 fs.writeFileSync(path.join(__dirname, "../src/fft.js"), fftExport);
